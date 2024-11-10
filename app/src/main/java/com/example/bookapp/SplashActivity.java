85ddde500cecbc90ffd2d2a4dcf4fa4fdf5f0983
tuan hoc code <tuandinhnguyen2944@gmail.com>
2024-11-11 01:12:54 +0700

    Commit-Tuanpro

--- app/src/main/java/com/example/bookapp/SplashActivity.java
+++ app/src/main/java/com/example/bookapp/SplashActivity.java
@@ -10,7 +10,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SplashActivity extends AppCompatActivity {
+public class  SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
